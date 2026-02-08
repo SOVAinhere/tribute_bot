bef30b240caff92c5da35982028135ee5c62c1ee
Admin <SOVAinhere@yandex.ru>
2026-02-09 01:21:57 +0300

    //

--- db.py
+++ db.py
@@ -1,3 +1,17 @@
 import aiosqlite
 
 DB_NAME = "bot.db"
+
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                plan TEXT,
+                subscription_until TEXT
+            )
+        """)
+        await db.commit()
+
+
